f05073d0d42be824310ee973905845893fa83265
joshpee22 <owobujoshua2017@gmail.com>
2023-03-17 08:52:35 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet at reverse
+ * main - Prints a hexadecimal string
  *
  * Return: Always (Success)
  */
-
 int main(void)
 {
 	char c;
 
-	for (c = 'z'; c >= 'a'; c--)
+	for (c = '0'; c <= '9'; c++)
+	{
 		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
 	putchar('\n');
+
 	return (0);
-}	
+}
