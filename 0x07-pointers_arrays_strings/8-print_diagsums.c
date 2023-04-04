2c0075df7315e518b7770f8e1f3526b569696e43
joshpee22 <owobujoshua2017@gmail.com>
2023-04-04 15:29:57 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,26 +1,28 @@
 #include "main.h"
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                
- * pirint_diagsums - Entry point                                                                            
- * of a square matrix of integers.                                                                                
- * @a: The matrix of integers.                                                                                                     
- * @size: The size of the matrix.                                                                                                  
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: (Success)
  */
-
 void print_diagsums(int *a, int size)
 {
-	int i;
+int sum1, sum2, y;
 
-	unsigned int sum, sum1;
+sum1 = 0;
+sum2 = 0;
 
-	sum = 0;
-	sum1 = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		sum += a[(size * i) + i];
-		sum1 += a[(size * (i + 1)) - (i + 1)];
-	}
-
-	printf("%d, %d\n", sum, sum1);
+for (y = 0; y < size; y++)
+{
+sum1 = sum1 + a[y * size + y];
 }
+
+for (y = size - 1; y >= 0; y--)
+{
+sum2 += a[y * size + (size - y - 1)];
+}
+
+printf("%d, %d\n", sum1, sum2);
+}
+
