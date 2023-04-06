9c99fb2cf4388ff69db4c9337e3f40904f3ee02c
joshpee22 <owobujoshua2017@gmail.com>
2023-04-06 11:29:23 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,20 +2,20 @@
 
 /**
  * power_operation - returns the natural square root of a number.
- * @n: input number.
+ * @a: input number.
  * @c: iterator.
  * Return: square root or -1.
  */
-int power_operation(int n, int c)
+int power_operation(int a, int c)
 {
-	if (c % (n / c) == 0)
+	if (c % (a / c) == 0)
 	{
-		if (c * (n / c) == n)
+		if (c * (a / c) == a)
 			return (c);
 		else
 			return (-1);
 	}
-	return (0 + power_operation(n, c + 1));
+	return (0 + power_operation(a, c + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number.
