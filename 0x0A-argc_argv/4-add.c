399af3734006a6d9d6409908976527a04168d1f1
joshpee22 <owobujoshua2017@gmail.com>
2023-04-12 09:25:10 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
- * main - add 2 positive numbers and print the result
- * @argc: argument count
- * @argv: argument vector, array of strings
- * Description: If no number is passed to program, print 0.
- * If one of the numbers contain non-digits, print Error.
- * Return: 1 if error, 0 if function runs properly.
+ * main - Print result of adding given arguments
+ * @argc: Number of arguments
+ * @argv: Arguments recieved
+ *
+ * Return: 0 on success, 1 if theres a nondigit arg
  */
+
 int main(int argc, char *argv[])
 {
-	int sum;
-	int count;
-	int i;
+	int total, i, j;
+	int num;
 
-	count = 1;
-	sum = 0;
-	if (argc == 1)
+	total = 0;
+	if (argc > 1)
 	{
-		printf("0\n");
-		return (0);
-	}
-	while (count < argc)
-	{
-		for (i = 0; argv[count][i] != '\0'; i++)
+		for (i = 1; argv[i]; i++)
 		{
-			if (!((argv[count][i])))
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+
+			num = atoi(argv[i]);
+			total += num;
 		}
-		sum += atoi(argv[count]);
-		count++;
 	}
-	printf("%d\n", sum);
+	printf("%d\n", total);
 	return (0);
 }
+
